3d49bf727107dd9e99c4b7de54135cb79378d54b
Ye <xiye@mac-xiye.local>
2016-06-28 22:55:28 -0400

    d3 force layout test

--- controllers/gallery.js
+++ controllers/gallery.js
@@ -2,18 +2,32 @@
  * Created by xiye on 6/28/16.
  */
 angular.module("galleryApp")
-    .controller("visCtrl", function($scope)
-{
+    .controller("visCtrl", function ($scope, $location) {
 
 
-    $scope.data = {
-        visuals: [
-            {name: "Visualizaiton #1", description: "DESC of Vis #1", category: "D3", createDate: "June 28, 2016"},
-            {name: "Visualizaiton #2", description: "DESC of Vis #2", category: "D3", createDate: "June 28, 2016"},
-            {name: "Visualizaiton #3", description: "DESC of Vis #3", category: "D3", createDate: "June 28, 2016"},
-            {name: "Visualizaiton #4", description: "DESC of Vis #4", category: "Google Chart", createDate: "June 28, 2016"},
-            {name: "Visualizaiton #5", description: "DESC of Vis #5", category: "Google Chart", createDate: "June 28, 2016"}
-        ]
-    };
-}
-);
+            $scope.data = {
+                visuals: [
+                    {name: "Visualizaiton #1", description: "DESC of Vis #1", category: "D3", createDate: "June 28, 2016"},
+                    {name: "Visualizaiton #2", description: "DESC of Vis #2", category: "D3", createDate: "June 28, 2016"},
+                    {name: "Visualizaiton #3", description: "DESC of Vis #3", category: "D3", createDate: "June 28, 2016"},
+                    {
+                        name: "Visualizaiton #4",
+                        description: "DESC of Vis #4",
+                        category: "Google Chart",
+                        createDate: "June 28, 2016"
+                    },
+                    {
+                        name: "Visualizaiton #5",
+                        description: "DESC of Vis #5",
+                        category: "Google Chart",
+                        createDate: "June 28, 2016"
+                    }
+                ]
+            };
+
+            $scope.go = function ( path ) {
+                $location.url = path;
+                //$location.replace();
+            };
+        }
+    );
